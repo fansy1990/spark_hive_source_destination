eb3245f943a2a70702ee7dc986e5b3de7a2550a1
fansy1990 <fansy1990@foxmail.com>
2016-11-29 22:42:44 +0800

    修改spark write hive的方式

--- src/main/scala/hiveinout/SparkReadWriteHive.scala
+++ src/main/scala/hiveinout/SparkReadWriteHive.scala
@@ -4,12 +4,14 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 /**
  * Created by fanzhe on 2016/11/27.
+ * 调用方式：
+ * spark-submit --class hiveinout.SparkReadWriteHive --master yarn --deploy-mode cluster --jars /usr/lib/hive/lib/datanucleus-core-3.2.10.jar --files /usr/lib/hive/conf/hive-site.xml spark_hive-1.0-SNAPSHOT.jar products tmp3
  */
 object SparkReadWriteHive {
 
   def main(args:Array[String]): Unit ={
-    if(args.length!=1){
-      println("SparkReadHive: <table_in> <columns> <table_out>")
+    if(args.length!=2){
+      println("SparkReadHive: <table_in> <table_out>")
       System.exit(1)
     }
     val conf = new SparkConf().setAppName("mySpark")
@@ -23,7 +25,7 @@ object SparkReadWriteHive {
     // data 可以是经过其他处理后的数据
     val tmpTableName = "tmp" + System.currentTimeMillis()
     data.registerTempTable(tmpTableName)
-    sqlContext.sql("create table "+ args(2) +" as select * from "+ tmpTableName)
+    sqlContext.sql("create table "+ args(1) +" as select * from "+ tmpTableName)
     sc.stop()
   }
 }
